524e8aefa5c537517916773684bcbd068f2a2dc9
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-09-14 07:54:40 -0400

    create categories actions

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::CategoriesController < ApplicationController
+
+    def index 
+        categories = Category.all
+        render json: categories.to_json(except: [:created_at, :updated_at])
+    end 
+
+    def create
+        category = Category.create(category_params)
+        render json: category 
+    end 
+
+    def destroy 
+        category = Category.find(params[:id])
+        if cateogry.destroy 
+            render json: {message: 'Category was deleted'}
+        end 
+    end 
+
+
 end
