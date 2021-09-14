88f6696639fcd2bb01595974fba764b9167d30df
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-09-14 08:06:24 -0400

    add actions to reviews_controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::ReviewsController < ApplicationController
+    def index 
+        reviews = Review.all 
+        render json: reviews.to_json(except: [:created_at, :updated_at])
+    end 
+
+    def create 
+        review = Review.create(review_params)
+        render json: review, except: [:created_at, :updated_at]
+    end 
+
+    def destroy 
+        review = Review.find(params[:id])
+        if review.destory 
+            render json: {message: 'Review was deleted'}
+        end 
+    end 
+
 end
