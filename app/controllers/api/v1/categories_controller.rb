ab7f273ea1210209e3b5781a9a77e9e64c42bc17
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-09-14 07:59:27 -0400

    add category params

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -17,5 +17,8 @@ class Api::V1::CategoriesController < ApplicationController
         end 
     end 
 
-
+    private
+    def category_params
+        params.require(:category).permit(:name, :description)
+    end 
 end
