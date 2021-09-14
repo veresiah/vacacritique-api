f51b6a30f0bf97fe2f806338c412068d1a23c394
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-09-14 09:03:21 -0400

    add category_id to reviews tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_113315) do
+ActiveRecord::Schema.define(version: 2021_09_14_130044) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_113315) do
     t.text "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
